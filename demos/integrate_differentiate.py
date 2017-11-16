34ae257302e852bb99df5a0c5f8ae1bcc1b425be
Dan Weatherill <danw@linux-ocs4.weatherill.dyndns.org>
2017-11-16 03:00:46 +0000

    all wave types implemented, nice and tidy

--- demos/integrate_differentiate.py
+++ demos/integrate_differentiate.py
@@ -10,18 +10,32 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, RadioButtons, Cursor
 from matplotlib.gridspec import GridSpec
 import numpy as np
-from math import sin, cos
+from math import sin, cos, modf, copysign
+
+def sign(x): return 1 if x >=0 else -1
 
 class DiffTangentDraw:
-    def __init__(self,ax,xs,fun,gradfun,**plotkwargs):
+    def __init__(self,ax,curveax,xs):
         self._ax =ax
         self._xs = xs
-        self._fun = fun
-        self._gradfun = gradfun
-        self._ys = [fun(_) for _ in xs]
-        self._line = ax.plot(xs,self._ys,**plotkwargs)[0]
+        self._curveax = curveax
         
-    def draw(self,x,d=20):
+    def update_funs(self,fun,dfun,ifun):
+        self._fun = fun
+        self._gradfun = dfun
+        self._ifun = ifun
+        self._dys = [dfun(_) for _ in self._xs]
+        self._iys = [ifun(_) for _ in self._xs]
+        self._ys = [fun(_) for _ in self._xs]
+        
+        if not hasattr(self,"_line"):
+            self._line = self._ax.plot(self._xs,self._ys,c="blue")[0]
+        else:
+            self._line.set_data(self._xs,self._ys)
+        
+        plt.gcf().canvas.draw_idle()
+        
+    def draw_tangent(self,x,d=20):
         m = self._gradfun(x)
         y0 = self._fun(x)
 
@@ -33,6 +47,7 @@ class DiffTangentDraw:
         else:
             self._gradline.set_data(xx,yy)
             
+    def draw_area(self,x):
         xsel = self._xs[self._xs < x]
         ysel = self._ys[:len(xsel)]
         
@@ -41,74 +56,94 @@ class DiffTangentDraw:
         else:
             self._integarea.remove()
             self._integarea = self._ax.fill_between(xsel,ysel,alpha=0.2,color="red")
-##        
+##  
+    def draw_curve(self,x,yvals,attrname,col):
+        xsel = self._xs[self._xs < x]
+        ysel = yvals[:len(xsel)]
+        if not hasattr(self,attrname):
+            setattr(self,attrname,self._curveax.plot(xsel,ysel,c=col)[0])
+        else:
+            getattr(self,attrname).set_data(xsel,ysel)
+                            
+    
     def move_event(self,event):
         if not event.inaxes:
             return
-        x,y = event.xdata, event.ydata
+        x = event.xdata
         
-        self.draw(x)
+        self.draw_tangent(x)
+        self.draw_area(x)
+        
+        self.draw_curve(x,self._dys,"_diffline","green")
+        self.draw_curve(x,self._iys,"_intline","red")
+        self.draw_curve(x,self._ys,"_origline","blue")
     
-        
+def setup_sine_wave(drawobj,ax,cax):
+    f = lambda x : sin(x)
+    gf = lambda x : cos(x)
+    ifun = lambda x : -cos(x)
+    drawobj.update_funs(f,gf,ifun)
+    cax.set_ylim(-2,2)
+    ax.set_ylim(-2,2)
+
+    
+def setup_square_wave(drawobj,ax,cax):
+    f = lambda x : 1 if modf(x / np.pi /2)[0] < 0.5 else -1    
+    pm = lambda x : 1 if modf(x/np.pi)[1] %2 == 0 else -1    
+    gf =  lambda x : pm(x) * 1000 if modf(x/np.pi)[0] < 0.01 else 0
+    ifun = lambda x : modf(x/np.pi/2)[0] if modf(x/np.pi/2)[0] < 0.5 else modf(3-x/np.pi/2)[0]
+    drawobj.update_funs(f,gf,ifun)
+    cax.set_ylim(-2,2)
+    ax.set_ylim(-2,2)
+
+
+def setup_tri_wave(drawobj,ax,cax):
+    m = lambda x : modf(x/np.pi/2)[0]
+    m2 = lambda x: modf(x/np.pi)[0]
+    f = lambda x : m2(x) -0.5 if m(x) < 0.5 else  (m2(-x)+0.5)
+    gf = lambda x: 1 / np.pi if m(x) < 0.5 else -(1/np.pi)
+    ifun = lambda x : (-0.5*m2(x) + m2(x)**2/2) if m(x) < 0.5 else (m2(-x)**2/2 + 0.5*m2(-x))
+    drawobj.update_funs(f,gf,ifun)
+    
+    cax.set_ylim(-0.5,0.5)
+    ax.set_ylim(-1,1)
+
 if __name__ == "__main__":
 
     NPOINTS = 1000
     omega = 1
     ts = np.linspace(0,4*np.pi,NPOINTS)
-    ys = np.sin(omega * ts)
-
-    dys = omega * np.cos(omega*ts)
-    iys = -1/(omega) * np.cos(omega*ts)
-
-    gs = GridSpec(2,1,height_ratios=[1,1])
 
     plt.close("all")
     fig,axs  = plt.subplots(2,1,sharex=True)
+    
+    rax = plt.axes([0.05,0.1,0.15,0.15])
+    radios = RadioButtons(rax,["sine","square","tri"])
+    
+    
     ax = axs[0]
     ax.set_xlim(0,max(ts))
     ax.set_ylim(-2,2)
 
-    axdiff = axs[1]
-
-    f = lambda x : sin(x)
-    gf = lambda x : cos(x)
-    
+    axdiff = axs[1]    
     curs = Cursor(ax,horizOn=False,vertOn=True,c="orange")
-    diffline = axdiff.plot([],[],c="green")[0]
-    intline = axdiff.plot([],[],c="red")[0]
-    origline = axdiff.plot([],[],c="blue")[0]
     axdiff.set_ylim(-2,2)
 
-    def draw_dif(ev):
-        if not ev.inaxes:
-            return
-        x = ev.xdata
-        
-        tsel= ts[ts <x]
-        ysel = dys[ts < x]
-        diffline.set_data(tsel,ysel)
-
-    def draw_int(ev):
-        if not ev.inaxes:
-            return
-        x = ev.xdata
-
-        tsel = ts[ts < x]
-        ysel = iys[ts < x]
-        intline.set_data(tsel,ysel)
-        
-    def draw_orig(ev):
-        if not ev.inaxes:
-            return
-        x = ev.xdata
-        tsel = ts[ts < x]
-        ysel = ys[ts < x]        
-        origline.set_data(tsel,ysel)
-
-    tang = DiffTangentDraw(ax,ts,f,gf)
+    tang = DiffTangentDraw(ax,axdiff,ts)
+#    setup_sine_wave(tang)
+#    setup_square_wave(tang)
     
+    
+    def choose_wave(name):
+        if name == "sine":
+            setup_sine_wave(tang,ax,axdiff)
+        elif name == "square":
+            setup_square_wave(tang,ax,axdiff)
+        elif name == "tri":
+            setup_tri_wave(tang,ax,axdiff)
+    
+    setup_sine_wave(tang,ax,axdiff)
+    radios.on_clicked(choose_wave)
     plt.connect("motion_notify_event",tang.move_event)
-    plt.connect("motion_notify_event",draw_dif)
-    plt.connect("motion_notify_event",draw_int)
-    plt.connect("motion_notify_event",draw_orig)
+
         
